f4e48c993f0ae3865707cf1051518f9b00106fb0
ErkhamAbadan <erkham012@gmail.com>
2023-12-03 11:56:26 +0700

    PPPPP

--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {    
-    cout << "hello world gfgfgfgf";
-    return 0;
+    cout << "Hello wolrd" << endl;
+    cout << "Selamat datang di tebak bahasa panda" << endl;
+    cout << "PERATURAN: " << endl;
+    cout <<""
 }
