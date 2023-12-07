97ee181a4fc413ec778b7f7a251d70072bf4b19c
Alvi399 <muhammadalvi.23339@mhs.unesa.ac.id>
2023-12-07 11:12:38 +0700

    BETA

--- main.cpp
+++ main.cpp
@@ -13,21 +13,16 @@ bool compareScores(const User &a, const User &b) {
     return a.score > b.score; // Mengurutkan secara menurun (dari skor tertinggi ke terendah)
 }
 
-
-void playGame() {
-    int skor = 0;
-    int hati = 3;
-    bool main = true;
-
-    levelOneGame(&hati,&main,&skor);
-    }
 //levelOne
-bool levelOneGame(int *heart, bool *play, int *skor) {
+bool levelOneGame(int *heart, int *skor, int *level) {
+    int progress_soal = 1,heart_local = *heart, skor_local = *skor, level_local = *level;
+    bool berhasil;
+    //input soal ke array
     ifstream baca ("database/data_soal1.txt");
     string bankSoal [3][2];
     string soal;
     int index = 0, line = 0;
-    while (baca >> soal )
+    while (getline(baca,soal))
     {
         line++;
         if (line%2 == 0)
@@ -38,9 +33,236 @@ bool levelOneGame(int *heart, bool *play, int *skor) {
             bankSoal[index][0] = soal;
         }
     }
-    
-
+    //pengguna berinteraksi dengan program
+    while (heart_local > 0)
+    {
+        bool soal1 = true,soal2 = true,soal3 = true;
+        while (soal1 == true)
+        {
+            system("cls");
+            cout << "Nyawa: " << heart_local <<"     Soal: " << progress_soal << "     Level: "<< level_local <<"     Skor: "<< skor_local<<endl;
+            string jawaban;
+            cout << bankSoal[0][0] << ": ";
+            cin >> jawaban;
+            if (jawaban == bankSoal[0][1])
+            {
+                skor_local += 50;
+                soal1 = false;
+                progress_soal++;
+                break;
+            } else {
+                heart_local--;
+            } 
+            if (heart_local  < 1)
+            {
+                soal1 = false;
+                soal2 = false;
+                soal3 = false;
+                break;
+            }
+        }
+        while (soal2 == true)
+        {
+            system("cls");
+            cout << "Nyawa: " << heart_local <<"     Soal: " << progress_soal << "     Level: "<< level_local <<"     Skor: "<< skor_local<<endl;
+            string jawaban;
+            cout << bankSoal[1][0] << ": ";
+            cin >> jawaban;
+            if (jawaban == bankSoal[1][1])
+            {
+                skor_local += 50;
+                soal2 = false;
+                progress_soal++;
+                break;
+            } else {
+                heart_local--;
+            }
+            if (heart_local  < 1)
+            {
+                soal1 = false;
+                soal2 = false;
+                soal3 = false;
+                break;
+            }
+            
+        }
+        while (soal3 == true)
+        {
+            system("cls");
+            cout << "Nyawa: " << heart_local <<"     Soal: " << progress_soal << "     Level: "<< level_local <<"     Skor: "<< skor_local<<endl;
+            string jawaban;
+            cout << bankSoal[2][0] << ": ";
+            cin >> jawaban;
+            if (jawaban == bankSoal[2][1])
+            {
+                skor_local += 50;
+                soal3 = false;
+                progress_soal++;
+                break;
+            } else {
+                heart_local--;
+            } 
+            if (heart_local < 1)
+            {
+                soal1 = false;
+                soal2 = false;
+                soal3 = false;
+                break;
+            }
+        }
+        if (heart_local > 0)
+        {
+            if (heart_local < 3)
+            {
+                heart_local++;
+            }
+            level_local++;
+            berhasil = true;
+            break;
+        } else {
+            berhasil = false;
+        }
+    }
+    *heart = heart_local;
+    *skor = skor_local;
+    *level = level_local;
+    return berhasil;
 }
+//levelTwo
+bool levelTwoGame(int *heart, int *skor, int *level) {
+    int progress_soal = 1,heart_local = *heart, skor_local = *skor, level_local = *level;
+    bool berhasil;
+    //input soal ke array
+    ifstream baca ("database/data_soal2.txt");
+    string bankSoal [3][2];
+    string soal;
+    int index = 0, line = 0;
+    while (getline(baca,soal))
+    {
+        line++;
+        if (line%2 == 0)
+        {
+            bankSoal[index][1] = soal;
+            index++;
+        } else {
+            bankSoal[index][0] = soal;
+        }
+    }
+    //pengguna berinteraksi dengan program
+    while (heart_local > 0)
+    {
+        bool soal1 = true,soal2 = true,soal3 = true;
+        while (soal1 == true)
+        {
+            system("cls");
+            cout << "Nyawa: " << heart_local <<"     Soal: " << progress_soal << "     Level: "<< level_local <<"     Skor: "<< skor_local<<endl;
+            string jawaban;
+            cout << bankSoal[0][0] << ": ";
+            cin >> jawaban;
+            if (jawaban == bankSoal[0][1])
+            {
+                skor_local += 50;
+                soal1 = false;
+                progress_soal++;
+                break;
+            } else {
+                heart_local--;
+            } 
+            if (heart_local  < 1)
+            {
+                soal1 = false;
+                soal2 = false;
+                soal3 = false;
+                break;
+            }
+        }
+        while (soal2 == true)
+        {
+            system("cls");
+            cout << "Nyawa: " << heart_local <<"     Soal: " << progress_soal << "     Level: "<< level_local <<"     Skor: "<< skor_local<<endl;
+            string jawaban;
+            cout << bankSoal[1][0] << ": ";
+            cin >> jawaban;
+            if (jawaban == bankSoal[1][1])
+            {
+                skor_local += 50;
+                soal2 = false;
+                progress_soal++;
+                break;
+            } else {
+                heart_local--;
+            }
+            if (heart_local  < 1)
+            {
+                soal1 = false;
+                soal2 = false;
+                soal3 = false;
+                break;
+            }
+            
+        }
+        while (soal3 == true)
+        {
+            system("cls");
+            cout << "Nyawa: " << heart_local <<"     Soal: " << progress_soal << "     Level: "<< level_local <<"     Skor: "<< skor_local<<endl;
+            string jawaban;
+            cout << bankSoal[2][0] << ": ";
+            cin >> jawaban;
+            if (jawaban == bankSoal[2][1])
+            {
+                skor_local += 50;
+                soal3 = false;
+                progress_soal++;
+                break;
+            } else {
+                heart_local--;
+            } 
+            if (heart_local < 1)
+            {
+                soal1 = false;
+                soal2 = false;
+                soal3 = false;
+                break;
+            }
+        }
+        if (heart_local > 0)
+        {
+            if (heart_local < 3)
+            {
+                heart_local++;
+            }
+            level_local++;
+            berhasil = true;
+            break;
+        } else {
+            berhasil = false;
+        }
+    }
+    *heart = heart_local;
+    *skor = skor_local;
+    *level = level_local;
+    return berhasil;
+}
+
+void playGame() {
+    int skor = 0;
+    int hati = 3;
+    int level = 1;
+    bool main1 =  levelOneGame(&hati,&skor,&level);
+    if (main1 == true)
+    {
+        bool main2 = levelTwoGame(&hati,&skor,&level);
+        if (main2 == true) {
+        
+        } else {
+            cout << "Your Failed\n";
+            cout << "your skor is " << skor;
+        }
+    } else {
+        cout << "Your Failed\n";
+        cout << "your skor is " << skor;
+    }
+    }
 void cekLeaderBoard(string nama = "a",int skor = 0, bool input = false) {
     if (input == true)
     {
@@ -145,12 +367,15 @@ int  menuGame() {
 
 int main(int argc, char const *argv[])
 {
-    string nama_user;    
+    //definisikan user
+    User pengguna;    
     cout << "Masukan nama pengguna: ";
-    cin >> nama_user;
+    cin >> pengguna.name;
+    pengguna.score = 0;
+
+    //user brinteraksi dengan opsi dalm game
     int opsi = menuGame();
     cout << opsi;
-
     switch (opsi)
     {
     case 1:
